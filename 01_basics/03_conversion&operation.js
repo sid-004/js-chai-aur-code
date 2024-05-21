39a22769d49cb59fb370026a52e74c4f8a187d1c
sid-004 <study.siddd@gmail.com>
2024-05-21 19:03:21 +0000

    updated

--- 01_basics/03_conversion&operation.js
+++ 01_basics/03_conversion&operation.js
@@ -26,5 +26,37 @@ let someNumber = 33
 
 let stringNumber = String(someNumber)
 
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+// ************************  OPERATIONS  ************************
+
+let value = 3
+let negValue = -value
+
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/2);
+// console.log(2%2);
+
+let str1 = "hello"
+let str2 = " Swayan"
+
+let str3 = str1 + str2
+
+// console.log(str3);
+
+// console.log("1" + 2 + 2);
+// console.log(1 + "2");
+// console.log( 1 + 2 + "2");
+
+let gameCounter = 100
+gameCounter++
+console.log(gameCounter);
+
+++gameCounter
+console.log(gameCounter);
